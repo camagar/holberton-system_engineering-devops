f1bf964c89de67de2a4ca310bd48a223c6b4cc22
camagar <mauricio@MAURICIO.localdomain>
2021-05-18 01:00:26 -0500

    count fixed

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,15 +7,17 @@ import requests
 base_url = 'http://reddit.com/r/{}/hot.json'
 
 
-def count_words(subreddit, word_list, hot_list=[], after=""):
+def count_words(subreddit, word_list):
     """all posts recursively"""
-    headers = {'User-agent': 'karenahv'}
+    headers = {'User-agent': 'camagar'}
     params = {'t': all, 'after': after}
     req = requests.get(base_url.format(subreddit), headers=headers,
                        params=params)
     if not req or req.status_code != 200:
         return None
     info = req.json()
+    hot_list = hot_list=[]
+    after=""
     for hot in info['data']['children']:
         hot_list.append(hot['data']['title'])
     after = info.get('data').get('after')
